66b4c1ba461b0eb8e4196a4920b5d936ef03f911
Douglas <jrdouglas98@gmail.com>
2022-12-20 01:04:33 -0400

    Change original code

---
+++ src/FirstApp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Fragment } from 'react'
+import { Button } from '@mui/material'
+
+export const FirstApp = () => {
+  return (
+<Fragment>
+
+      <Button> 
+        First APP
+      </Button>
+    
+</Fragment>
+    
+  )
+}
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FirstApp } from './FirstApp';
+import MyPersonalProfile from './MyPersonalProfile';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <MyPersonalProfile/>
+        <FirstApp/>
+    </React.StrictMode>
+);
